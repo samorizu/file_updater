b8377204827c9515fb9328a85052dd4d0e0773ee
Luke Matarazzo <ljm3103@rit.edu>
2013-09-27 15:42:16 -0400

    Updating help information and comments, commenting out lines that aren't ready

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Authors: Jackson Sadowski and Luke Matarazzo
-#Purpose: Grab files via FTP, update them the way the user defines, and reupload them
+#Purpose: Grab files via SFTP or FTP, update them the way the user defines, and reupload them
 
 if [ $# -lt 1 ]; then
   echo "Usage: ./update.sh [CONFIGFILE]"
@@ -10,18 +10,20 @@ fi
 if [ $1 = "--help" ]; then
   echo "Printing help information"
   echo "CONFIGFILE - Text file containing each file on the remote FTP server that needs to be updated. Each file should be on a separate line."
+  echo "-f - Specifies the use of FTP rather than SFTP. SFTP is the default protocol for this program."
+  exit 0
 fi
 
 
 
 #setting the config file to a variable
-file=$1
+#file=$1
 
 #getting a line 
-line=`cat $file | head -1`
+#line=`cat $file | head -1`
 
 #checking for a line
-if[ "$line" = "" ]; then
-   echo "No more files"
+#if[ "$line" = "" ]; then
+#   echo "No more files"
 
 
