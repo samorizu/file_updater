ffe51a8e3a76c9fa8dce3501f1cc0f82440265c3
Jackson <jps5374@rit.edu>
2013-09-27 15:22:23 -0400

    Adding feature to read the config file

--- update.sh
+++ update.sh
@@ -11,3 +11,17 @@ if [ $1 = "--help" ]; then
   echo "Printing help information"
   echo "CONFIGFILE - Text file containing each file on the remote FTP server that needs to be updated. Each file should be on a separate line."
 fi
+
+
+
+#setting the config file to a variable
+file=$1
+
+#getting a line 
+line=`cat $file | head -1`
+
+#checking for a line
+if[ "$line" = "" ]; then
+   echo "No more files"
+
+
